6f2f86797fe098b8918b89170ccef14db158c900
RadioZahra <zahra.khanjani@gmail.com>
2024-08-02 11:21:03 +0330

    Create maxSubarraySum.js

---
+++ maxSubarraySum.js
@@ -0,0 +1,21 @@
+/** 
+Sliding Window - maxSubarray Sum
+
+Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+maxsubarraySum([100,200,300,4001, 2) // 700
+maxSubarraySum([1,4,2,10,23,3,1,0,201, 4) // 39
+maxSubarraySum([-3,4,0,-2,6, -11, 2) // 5
+maxSubarraySum([3,-2,7,-4,1,-1,4,-2,11,2) // 5
+maxSubarraySum([2,31, 3) // null
+
+Constraints:
+Time Complexity - O(N)
+Space Complexity - 0(1)
+**/
+
+function maxSubarraySum(){
+  // add whatever parameters you deem necessary - good luck!
+}
