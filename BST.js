1263c01fb7267bd4a8391def84e80516f4ce3ff4
RadioZahra <zahra.khanjani@gmail.com>
2024-08-02 11:27:20 +0330

    Create BST.js

---
+++ BST.js
@@ -0,0 +1,48 @@
+/**
+Please implement the find and contains to the following Binary Search Tree
+**/
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+    find(value){
+        // find the node based on the given value
+        // return value should be the found node or undefined
+    }
+    contains(value){
+        // return true or false if the contains the given value or not
+    }
+}
