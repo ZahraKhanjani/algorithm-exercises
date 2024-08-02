d366d870946e3d4f9d1174fac245189eb6668f2f
RadioZahra <zahra.khanjani@gmail.com>
2024-08-02 11:29:55 +0330

    Create queue.js

---
+++ queue.js
@@ -0,0 +1,35 @@
+/**
+  Please add dequeue function to the following Queue 
+**/
+
+
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    enqueue(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue(){
+        // return null or a value
+    }
+}
