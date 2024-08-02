891d856d5c8419c9512be74f798916b0d0b59940
RadioZahra <zahra.khanjani@gmail.com>
2024-08-02 11:19:45 +0330

    Create findLongestSubstring.js

---
+++ findLongestSubstring.js
@@ -0,0 +1,18 @@
+/** 
+Sliding Window - findLongestSubstring
+Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+findLongestSubstring(*') // 0
+findLongestSubstring('rithmschool') // 7
+findLongestSubstring('thisisawesome') // 6]
+findLongestSubstring('thecatinthehat') // 7
+findLongestSubstring('bbbbbb') // 1
+findLongestSubstring('longestsubstring') // 8|
+findLongestSubstring('thisishowwedoit') // 6
+
+Time Complexity - O(n)
+**/
+
+function findLongestSubstring(){
+  // add whatever parameters you deem necessary - good luck!
+}
