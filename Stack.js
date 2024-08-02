e60cb23d21b034de404c5df834b4021e1f28ba66
RadioZahra <zahra.khanjani@gmail.com>
2024-08-02 11:40:16 +0330

    Create Stack.js

---
+++ Stack.js
@@ -0,0 +1,33 @@
+// implement the pop functionality
+
+
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    pop(){
+        // code here
+    }
+}
