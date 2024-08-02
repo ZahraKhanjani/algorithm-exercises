6cc5f5f59d46a93f78967bb7a845454b32a98d8c
RadioZahra <zahra.khanjani@gmail.com>
2024-08-02 11:38:21 +0330

    Create DoublyLinkedList.js

---
+++ DoublyLinkedList.js
@@ -0,0 +1,42 @@
+/**
+Complete the following DoublyLinkedList
+**/
+
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        // code here
+    } 
+    pop(){
+        // code here
+    }
+    shift(){
+        // code here
+    }
+    unshift(val){
+        // code here
+    }
+    get(index){
+        // code here
+    }
+    set(index, val){
+      // code here
+    }
+    insert(index, val){
+      // code here
+    }
+}
