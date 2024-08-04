fca732f558dbc2e29bbee18f5d8213d07bcad997
Hamed Nami <hamed.n@smartx.ir>
2024-08-04 11:19:51 +0330

    fix averagePair.js sample input

--- averagePair.js
+++ averagePair.js
@@ -6,11 +6,11 @@ Bonus Constraints:
 Time: O(N)
 Space: 0(1)
 
-Sample Input:
-averagePair ([1,2,31,2.5) // true 
-averagePair ([1,3,3,5,6,7,10,12,191,8) // true 
-averagePair ([-1,0,3,4,5,6], 4.1) // false 
-averagePair([1,4) // false
+ Sample Input:
+ averagePair ([1,2,3],2.5) // true
+ averagePair ([1,3,3,5,6,7,10,12,19],8) // true
+ averagePair ([-1,0,3,4,5,6], 4.1) // false
+ averagePair([],4) // false
 **/
 
 function averagePair(){
