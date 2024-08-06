86aaec895b6f49278d067def9418f3cbaeb7e9d5
Hamed Nami <hamed.n@smartx.ir>
2024-08-06 11:01:12 +0330

    resolve some exersices

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -1,18 +1,39 @@
 /**
-Frequency Counter / Multiple Pointers - areThereDuplicates
-Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
-Examples:
-areThereDuplicates(1, 2, 3) // false areThereDuplicates(1, 2, 2) // true areThereDuplicates('a', 'b', 'c', 'a') // true
+ Frequency Counter / Multiple Pointers - areThereDuplicates
+ Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+ Examples:
+ areThereDuplicates(1, 2, 3) // false areThereDuplicates(1, 2, 2) // true areThereDuplicates('a', 'b', 'c', 'a') // true
 
-Restrictions:
-Time - O(n)
-Space - O(n)
+ Restrictions:
+ Time - O(n)
+ Space - O(n)
 
-Bonus:
-Time - O(n log n)
-Space - O(1)
-**/
 
-function areThereDuplicates() {
-  // good luck. (supply any arguments you deem necessary.)
+ Bonus:
+ Time - O(n log n)
+ Space - O(1)
+ **/
+
+function areThereDuplicates(...arguments) {
+    const resultObject = {}
+    for (let item of arguments){
+        if (resultObject[item]){
+            return true
+        }
+        resultObject[item] = 1
+    }
+    return false
 }
+
+//Bonus Time=> O(nlogn) Space=> O(1)
+
+function refactoredAreThereDuplicate (...arguments) {
+    arguments.sort()
+
+    for (let i =0; i< arguments.length;i++){
+        if (arguments[i] === arguments[i+1])
+            return true
+    }
+    return false
+}
+
--- isSubsequence.js
+++ isSubsequence.js
@@ -1,10 +1,10 @@
-/** 
+/**
 Multiple Pointers - isSubsequence
 Write a function called is Subsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
 
 Examples:
-isSubsequence('hello', 'hello world'); // true 
-isSubsequence('sing', 'sting'); // true 
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
 isSubsequence('abc', 'abracadabra'); // true
 isSubsequence('abc', 'acb'); // false (order matters)
 
@@ -15,6 +15,16 @@ Space Complexity - 0(1)
 
 **/
 
-function isSubsequence() {
-  // good luck. Add any arguments you deem necessary.
+function isSubsequence(firstString,secondString) {
+    let firstPointer = 0
+
+    if (firstString.length > secondString.length) return false
+
+    for (let i = 0; i< secondString.length;i++){
+        if (firstString[firstPointer] ===secondString[i] ){
+            firstPointer ++
+        }
+    }
+
+    return firstPointer === firstString.length;
 }
